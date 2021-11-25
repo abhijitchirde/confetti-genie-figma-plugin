25c7f9eb0e42a5bc53814682568e748a451c4d1a
Abhijit Chirde <chirde.abhi@gmail.com>
2021-11-25 10:34:16 +0530

    Update to UI and code file
    - Increased values of range
    - Improved the codeJS block

--- code.js
+++ code.js
@@ -10,13 +10,13 @@ const colors = [
     { r: 0.863, g: 0.738, b: 0.98 },
     { r: 0.99, g: 0.621, b: 0.109 },
     { r: 0.617, g: 0.93, b: 0.102 },
-    { r: 0.482, g: 0.38, b: 1 },
-    { r: 1, g: 0, b: 1 },
+    { r: 0.46, g: 0.374, b: 0.996 },
+    { r: 1, g: 0.13, b: 0.985 },
     { r: 0.964, g: 0.144, b: 0.519 },
     { r: 0.281, g: 0.046, b: 0.656 },
     { r: 0.296, g: 0.785, b: 0.937 },
-    { r: 0.949, g: 0.282, b: 0.133 },
-    { r: 1, g: 0.921, b: 0 },
+    { r: 0.96, g: 0.277, b: 0.129 },
+    { r: 1, g: 0.93, b: 0.21 },
     { r: 0.730, g: 0.242, b: 0.011 },
 ];
 //Crate array containing opacity values. This can also be achieved by using a randomg 0-1 number generator. Having distinct values will create less variation
@@ -35,7 +35,7 @@ function runPlugin() {
             //If message is create-confetti
             if (msg.type === 'create-confetti') {
                 //calling confetti function
-                drawConfetti(node, number);
+                generateConfetti(node, number);
             }
         }
     };
@@ -58,29 +58,29 @@ function checkLayerStatus() {
 }
 ;
 //defining confetti function
-function drawConfetti(currentNode, count) {
+function generateConfetti(currentNode, count) {
     const width = currentNode.width;
     const height = currentNode.height;
-    //creating randomRange and randomColor functions. These can be defined globally using normal definition too
-    // const randomRange = (low, high) => Math.floor(Math.random() * high) + low;
-    // const randomColor = () => colors[Math.floor(Math.random() * colors.length)];
+    //creating numBetween and setColor functions. These can be defined globally using normal definition too
+    // const numBetween = (low, high) => Math.floor(Math.random() * high) + low;
+    // const setColor = () => colors[Math.floor(Math.random() * colors.length)];
     //add rectangles
     for (let i = 0; i < count; i++) {
         //create a rectangle
         const rect = figma.createRectangle();
         //Assign a random width and height to rectangle
-        const w = randomRange(width * 0.005, width * 0.03);
-        const h = randomRange(height * 0.005, height * 0.03);
+        const w = numBetween(width * 0.005, width * 0.03);
+        const h = numBetween(height * 0.005, height * 0.03);
         rect.resize(w, h);
         //randomly position rectangle within the frame
-        rect.x = randomRange(0, width);
-        rect.y = randomRange(0, height);
+        rect.x = numBetween(0, width);
+        rect.y = numBetween(0, height);
         //set a random color
         rect.fills = [
             {
                 type: 'SOLID',
-                color: randomColor(),
-                opacity: randomOpacity(),
+                color: setColor(),
+                opacity: setOpacity(),
             },
         ];
         currentNode.appendChild(rect);
@@ -90,18 +90,18 @@ function drawConfetti(currentNode, count) {
         //create a rectangle
         const ell = figma.createEllipse();
         //Assign a random width and height to rectangle
-        const w = randomRange(width * 0.005, width * 0.01);
+        const w = numBetween(width * 0.005, width * 0.01);
         const h = w; //keeping width and heights same to have circles
         ell.resize(w, h);
         //randomly position rectangle within the frame
-        ell.x = randomRange(0, width);
-        ell.y = randomRange(0, height);
+        ell.x = numBetween(0, width);
+        ell.y = numBetween(0, height);
         //set a random color
         ell.fills = [
             {
                 type: 'SOLID',
-                color: randomColor(),
-                opacity: randomOpacity(),
+                color: setColor(),
+                opacity: setOpacity(),
             },
         ];
         currentNode.appendChild(ell);
@@ -110,53 +110,53 @@ function drawConfetti(currentNode, count) {
     for (let i = 0; i < count / 2; i++) {
         //create a star
         const star = figma.createStar();
-        star.pointCount = randomRange(4, 5);
+        star.pointCount = numBetween(4, 7);
         //Assign a random width and height to rectangle
-        const w = randomRange(width * 0.01, width * 0.02);
+        const w = numBetween(width * 0.01, width * 0.02);
         const h = w; //keeping width and heights same to have circles
         star.resize(w, h);
         //randomly position rectangle within the frame
-        star.x = randomRange(0, width);
-        star.y = randomRange(0, height);
+        star.x = numBetween(0, width);
+        star.y = numBetween(0, height);
         //set a random color
         star.fills = [
             {
                 type: 'SOLID',
-                color: randomColor(),
-                opacity: randomOpacity(),
+                color: setColor(),
+                opacity: setOpacity(),
             },
         ];
         currentNode.appendChild(star);
     }
-    //add triangles/polygons
+    //add polygons
     for (let i = 0; i < count / 3; i++) {
         //create a star
         const poly = figma.createPolygon();
-        poly.pointCount = randomRange(3, 5);
+        poly.pointCount = numBetween(3, 7);
         //Assign a random width and height to rectangle
-        const w = randomRange(width * 0.005, width * 0.01);
+        const w = numBetween(width * 0.005, width * 0.01);
         const h = w; //keeping width and heights same to have circles
         poly.resize(w, h);
         //randomly position rectangle within the frame
-        poly.x = randomRange(0, width);
-        poly.y = randomRange(0, height);
+        poly.x = numBetween(0, width);
+        poly.y = numBetween(0, height);
         //set a random color
         poly.fills = [
             {
                 type: 'SOLID',
-                color: randomColor(),
-                opacity: randomOpacity(),
+                color: setColor(),
+                opacity: setOpacity(),
             },
         ];
         currentNode.appendChild(poly);
     }
 }
-//define function for getting a number between random range
-function randomRange(low, high) {
-    return Math.floor(Math.random() * high) + low;
+//define function for getting a number between a random range
+function numBetween(low, high) {
+    return Math.floor(Math.random() * (high - low)) + low;
 }
 //define a function to select a random color    //Made function to return random combinations rather than previous values from array
-function randomColor() {
+function setColor() {
     //getting from this array makes bright colors. Better to use for confetti
     return colors[Math.floor(Math.random() * colors.length)];
     //getting from this random values tend to make all the dark and bright colors. Little dull to use
@@ -167,7 +167,7 @@ function randomColor() {
     // }
 }
 //define a function to select a random color    //Made function to return random combinations rather than previous values from array
-function randomOpacity() {
+function setOpacity() {
     //getting from this array makes bright colors. Better to use for confetti
     return opacity[Math.floor(Math.random() * opacity.length)];
     //getting from this random values tend to make all the dark and bright colors. Little dull to use
--- code.ts
+++ code.ts
@@ -12,13 +12,13 @@ const colors = [
   { r: 0.863, g: 0.738, b: 0.98 },
   { r: 0.99, g: 0.621, b: 0.109 },
   { r: 0.617, g: 0.93, b: 0.102 },
-  { r: 0.482, g: 0.38, b: 1 },
-  { r: 1, g: 0, b: 1 },
+  { r: 0.46, g: 0.374, b: 0.996 },
+  { r: 1, g: 0.13, b: 0.985 },
   { r: 0.964, g: 0.144, b: 0.519 },
   { r: 0.281, g: 0.046, b: 0.656 },
   { r: 0.296, g: 0.785, b: 0.937 },
-  { r: 0.949, g: 0.282, b: 0.133 },
-  { r: 1, g: 0.921, b: 0 },
+  { r: 0.96, g: 0.277, b: 0.129 },
+  { r: 1, g: 0.93, b: 0.21 },
   { r: 0.730, g: 0.242, b: 0.011 },
 ];
 
@@ -48,7 +48,7 @@ function runPlugin(){
         if(msg.type === 'create-confetti'){
 
           //calling confetti function
-          drawConfetti(node, number);
+          generateConfetti(node, number);
         }
       }
     }
@@ -76,14 +76,14 @@ function checkLayerStatus(){
 
 
 //defining confetti function
-function drawConfetti(currentNode, count){
+function generateConfetti(currentNode, count){
 
   const width = currentNode.width;
   const height = currentNode.height;
 
-  //creating randomRange and randomColor functions. These can be defined globally using normal definition too
-  // const randomRange = (low, high) => Math.floor(Math.random() * high) + low;
-  // const randomColor = () => colors[Math.floor(Math.random() * colors.length)];
+  //creating numBetween and setColor functions. These can be defined globally using normal definition too
+  // const numBetween = (low, high) => Math.floor(Math.random() * high) + low;
+  // const setColor = () => colors[Math.floor(Math.random() * colors.length)];
 
 
   //add rectangles
@@ -92,20 +92,20 @@ function drawConfetti(currentNode, count){
     const rect = figma.createRectangle();
 
     //Assign a random width and height to rectangle
-    const w = randomRange(width * 0.005, width * 0.03);
-    const h = randomRange(height * 0.005, height * 0.03);
+    const w = numBetween(width * 0.005, width * 0.03);
+    const h = numBetween(height * 0.005, height * 0.03);
     rect.resize(w,h);
 
     //randomly position rectangle within the frame
-    rect.x = randomRange(0, width);
-    rect.y = randomRange(0, height);
+    rect.x = numBetween(0, width);
+    rect.y = numBetween(0, height);
 
     //set a random color
     rect.fills = [
       {
         type: 'SOLID',
-        color: randomColor(),
-        opacity: randomOpacity(),
+        color: setColor(),
+        opacity: setOpacity(),
       },
     ];
 
@@ -120,20 +120,20 @@ function drawConfetti(currentNode, count){
     const ell = figma.createEllipse();
 
     //Assign a random width and height to rectangle
-    const w = randomRange(width * 0.005, width * 0.01);
+    const w = numBetween(width * 0.005, width * 0.01);
     const h = w;  //keeping width and heights same to have circles
     ell.resize(w,h);
 
     //randomly position rectangle within the frame
-    ell.x = randomRange(0, width);
-    ell.y = randomRange(0, height);
+    ell.x = numBetween(0, width);
+    ell.y = numBetween(0, height);
 
     //set a random color
     ell.fills = [
       {
         type: 'SOLID',
-        color: randomColor(),
-        opacity: randomOpacity(),
+        color: setColor(),
+        opacity: setOpacity(),
       },
     ];
 
@@ -146,23 +146,23 @@ function drawConfetti(currentNode, count){
     //create a star
     const star = figma.createStar();
 
-    star.pointCount = randomRange(4,5);
+    star.pointCount = numBetween(4,7);
 
     //Assign a random width and height to rectangle
-    const w = randomRange(width * 0.01, width * 0.02);
+    const w = numBetween(width * 0.01, width * 0.02);
     const h = w;  //keeping width and heights same to have circles
     star.resize(w,h);
 
     //randomly position rectangle within the frame
-    star.x = randomRange(0, width);
-    star.y = randomRange(0, height);
+    star.x = numBetween(0, width);
+    star.y = numBetween(0, height);
 
     //set a random color
     star.fills = [
       {
         type: 'SOLID',
-        color: randomColor(),
-        opacity: randomOpacity(),
+        color: setColor(),
+        opacity: setOpacity(),
       },
     ];
 
@@ -170,28 +170,28 @@ function drawConfetti(currentNode, count){
 
   }
 
-  //add triangles/polygons
+  //add polygons
   for(let i = 0; i < count/3; i++){
     //create a star
     const poly= figma.createPolygon();
 
-    poly.pointCount = randomRange(3,5);
+    poly.pointCount = numBetween(3,7);
 
     //Assign a random width and height to rectangle
-    const w = randomRange(width * 0.005, width * 0.01);
+    const w = numBetween(width * 0.005, width * 0.01);
     const h = w;  //keeping width and heights same to have circles
     poly.resize(w,h);
 
     //randomly position rectangle within the frame
-    poly.x = randomRange(0, width);
-    poly.y = randomRange(0, height);
+    poly.x = numBetween(0, width);
+    poly.y = numBetween(0, height);
 
     //set a random color
     poly.fills = [
       {
         type: 'SOLID',
-        color: randomColor(),
-        opacity: randomOpacity(),
+        color: setColor(),
+        opacity: setOpacity(),
       },
     ];
 
@@ -201,13 +201,13 @@ function drawConfetti(currentNode, count){
 
 }
 
-//define function for getting a number between random range
-function randomRange(low, high){
-  return Math.floor(Math.random() * high) + low;
+//define function for getting a number between a random range
+function numBetween(low, high){
+  return Math.floor(Math.random() * (high-low)) + low;
 }
 
 //define a function to select a random color    //Made function to return random combinations rather than previous values from array
-function randomColor(){
+function setColor(){
   //getting from this array makes bright colors. Better to use for confetti
   return colors[Math.floor(Math.random() * colors.length)];
 
@@ -220,7 +220,7 @@ function randomColor(){
 }
 
 //define a function to select a random color    //Made function to return random combinations rather than previous values from array
-function randomOpacity(){
+function setOpacity(){
   //getting from this array makes bright colors. Better to use for confetti
   return opacity[Math.floor(Math.random() * opacity.length)];
 
